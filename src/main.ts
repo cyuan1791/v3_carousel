a389ac4602ddf9e5d9e27395a2d90c79e21513a6
Mark Yuan <mark@webasone.com>
2025-09-14 19:48:55 +0000

    update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from "vue";
 // @ts-ignore
 import App from "./App.vue";
-import router from "./router";
 
 let myWindow = window as any;
 
 const app = createApp(App);
-app.use(router);
 app.mount("#" + myWindow.asoneId);
